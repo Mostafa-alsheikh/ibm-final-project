725b518593c144960abd143fc805d68973e8ab10
Mostafa Mohamed <M.Mohamed2262@nu.edu.eg>
2025-09-11 01:52:21 +0300

    Add simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+# Simple Interest Calculation
+echo "Enter Principal:"
+read P
+echo "Enter Rate:"
+read R
+echo "Enter Time:"
+read T
+SI=$((P*R*T/100))
+echo "Simple Interest: $SI"
